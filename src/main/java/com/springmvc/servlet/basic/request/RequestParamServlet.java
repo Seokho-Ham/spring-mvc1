5b12210faa67f6ab793c176e5d8dea6aaa82a8c7
함석호 <kalejuice02@gmail.com>
2022-04-03 15:35:13 +0900

    feat : HttpRequestServlet 연습

---
+++ src/main/java/com/springmvc/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,30 @@
+package com.springmvc.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name="requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getParameterNames().asIterator()
+                .forEachRemaining(param -> System.out.println(param + "=" + request.getParameter(param)));
+
+        System.out.println("[단일 파라미터 조회]");
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        String age = request.getParameter("age");
+        System.out.println("age = " + age);
+    }
+}
