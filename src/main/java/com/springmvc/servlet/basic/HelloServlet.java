d78d6d1979624736ad960a9e6ba5021f77a7cb83
함석호 <kalejuice02@gmail.com>
2022-04-03 14:21:04 +0900

    HelloServlet

---
+++ src/main/java/com/springmvc/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.springmvc.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding(String.valueOf(StandardCharsets.UTF_8));
+        response.getWriter().write("hello " + username);
+    }
+}
