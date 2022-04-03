6c10503a2eaad7cdf840bd4f0d751e668afb422b
함석호 <kalejuice02@gmail.com>
2022-04-03 10:59:49 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/springmvc/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
