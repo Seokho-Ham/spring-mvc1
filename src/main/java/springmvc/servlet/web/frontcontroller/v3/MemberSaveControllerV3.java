03b5f885f959a3d8b1f5618b8bd32b7d4c1dfd83
함석호 <kalejuice02@gmail.com>
2022-04-18 23:36:41 +0900

    MvcController Version 3

---
+++ src/main/java/springmvc/servlet/web/frontcontroller/v3/MemberSaveControllerV3.java
@@ -0,0 +1,26 @@
+package springmvc.servlet.web.frontcontroller.v3;
+
+import springmvc.servlet.domain.member.Member;
+import springmvc.servlet.domain.member.MemberRepository;
+import springmvc.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member);
+
+        return mv;
+    }
+}
